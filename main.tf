18d0063a254380775057d5f8bec1e4765be77f05
luth <88308177+luthpg@users.noreply.github.com>
2026-07-28 13:59:47 +0900

    Set default required status checks contexts
    Add default status checks context if not specified in JSON.

--- main.tf
+++ main.tf
@@ -48,4 +48,6 @@ module "github_repositories" {
 
   repo_name   = each.value.name
   description = lookup(each.value, "description", "")
+  # JSON側で指定があればそれを使い、無ければデフォルト["build-and-test"]を使う
+  required_status_checks_contexts = lookup(each.value, "required_status_checks_contexts", ["build-and-test"])
 }
